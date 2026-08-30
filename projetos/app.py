a241b29b35b5a7062c778d988d2722df5d21bea1
Maria Fernanda F. A. <mafezinha2008@gmail.com>
2026-08-30 16:54:15 -0300

    Include cost estimation in monthly consumption calculation
    Added estimated cost calculation to monthly consumption.

--- projetos/app.py
+++ projetos/app.py
@@ -2,6 +2,7 @@ aparelho = input("Informe qual o seu aparelho: ")
 potencia = float(input ("Informe a potencia do seu aparelho em watts (W): "))
 tempo_uso = float(input ("Informe o tempo medio de uso diario do aparelho (em horas): "))
 
+#Calculo de consumo e custo
 
 consumoMensal = potencia * tempo_uso * 30 / 1000
 custoEstimado = consumoMensal * 0.75
